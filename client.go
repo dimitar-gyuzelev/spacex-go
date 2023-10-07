6486aea832b2280eebcf61471914faf712472889
Dimitar Gyuzelev <dimitar-gyuzelev@users.noreply.github.com>
2023-10-07 22:00:45 +0100

    added new apis

--- client.go
+++ client.go
@@ -5,14 +5,21 @@ import (
 	"github.com/dimitar-gyuzelev/spacex-go/v4clients"
 )
 
-type Client struct {
+type ClientV4 struct {
 	v4.CapsulesAPI
 	v4.CoresAPI
+	v4.DragonsAPI
+	v4.HistoryAPI
+	v4.LandpadsAPI
 }
 
-func NewClient(baseURL string) Client {
-	return Client{
+func NewClient(baseURL string) ClientV4 {
+	baseURL = baseURL + v4.VersionPrefix
+	return ClientV4{
 		CapsulesAPI: v4clients.NewCapsulesAPI(baseURL),
 		CoresAPI:    v4clients.NewCoresAPI(baseURL),
+		DragonsAPI:  v4clients.NewDragonsAPI(baseURL),
+		HistoryAPI:  v4clients.NewHistoryAPI(baseURL),
+		LandpadsAPI: v4clients.NewLandpadsAPI(baseURL),
 	}
 }
